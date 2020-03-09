a8ed40fe8843a2a9f2e84ca64d163291d07b8db8
Kirk Tolliver <kirktolliver28@gmail.com>
2020-03-08 22:10:01 -0500

    Added StarWars

--- starwars/src/components/StarWars.js
+++ starwars/src/components/StarWars.js
@@ -1,1 +1,38 @@
 //This is a Starwars component!
+const Container = styled.div`
+	height: 90vh;
+	width: 50vw;
+	margin: 25px auto;
+	overflow: auto;
+	background-color: rgba(0,0,0,0.30)
+`;
+
+const StarWars = () => {
+
+	const [characters, setCharacters] = useState([]);
+
+	useEffect(() => {
+		Axios.get('https://swapi.co/api/people/')
+		.then(res => {
+			setCharacters(res.data.results);			
+		})
+		.catch(err => console.log('Get request failed', err))
+	}, [])
+
+	return (
+		<Container>
+			{characters.map((data, index) => (
+	            <Card key = {index}
+                name = {data.name}
+                height = {data.height}
+                birthDate = {data.birth_year}
+                gender = {data.gender}
+                species = {data.species}
+                films = {data.films.length}/>
+	        ))}
+	    </Container>
+	)
+
+}
+
+export default StarWars; 
